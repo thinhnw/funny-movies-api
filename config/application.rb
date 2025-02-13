9a16f2b332a579c78392b1c87f5920890d7806af
thinhnw <thinhnv0412@gmail.com>
2025-02-13 16:40:17 +0700

    Add Sidekiq Web UI

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module FunnyMoviesApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Session::CookieStore, key: "_app_session_key"
   end
 end
