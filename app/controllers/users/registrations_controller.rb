3439a3a7ca03f620ed6fe2021ff63c99610d0c4b
thinhnw <thinhnv0412@gmail.com>
2025-02-15 21:54:59 +0700

    Disable new, edit, update, destroy, cancel actions in RegistrationController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,38 +7,38 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    head :not_found
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    head :not_found
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    head :not_found
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    head :not_found
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def cancel
+    head :not_found
+  end
 
   # protected
 
@@ -60,5 +60,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
+  # end
+  #
+  # private
+
+  # def respond_with(resource, _opts = {})
+  #   if request.method == "POST"
+  #     if resource.persisted?
+  #       render json: {
+  #         status: { code: 201, message: "Signed up sucessfully." },
+  #         data: {
+  #           token: current_token
+  #         }
+  #       }, status: :created
+  #     else
+  #       render json: {
+  #         status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+  #       }, status: :unprocessable_entity
+  #     end
+  #   end
   # end
 end
