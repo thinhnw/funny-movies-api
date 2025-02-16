75a1c07daac74bc46c5031d7271a6d56483af23b
thinhnw <thinhnv0412@gmail.com>
2025-02-16 15:50:05 +0700

    Add validation tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+  describe "validations" do
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to allow_value('foo@bar.com').for(:email) }
+    it { is_expected.to_not allow_value('foo@').for(:email) }
+    it { is_expected.to_not allow_value('@bar.com').for(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 end
