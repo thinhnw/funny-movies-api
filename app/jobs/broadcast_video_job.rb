99c53f2d9f70066fac446257aa59bce99736361a
thinhnw <thinhnv0412@gmail.com>
2025-02-18 01:32:25 +0700

    Change broadcast format to json

--- app/jobs/broadcast_video_job.rb
+++ app/jobs/broadcast_video_job.rb
@@ -2,6 +2,6 @@ class BroadcastVideoJob
   include Sidekiq::Job
 
   def perform(video)
-    ActionCable.server.broadcast("notification_channel", video)
+    ActionCable.server.broadcast("notification_channel", JSON.parse(video))
   end
 end
