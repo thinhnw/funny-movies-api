102c897cb2f0afc56da479bf1115f64e0a2a4207
thinhnw <thinhnv0412@gmail.com>
2025-02-17 16:20:11 +0700

    Refactor RegistrationController, SessionsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,83 +1,10 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :render_not_found, only: [ :new, :edit, :update, :destroy, :cancel ]
 
-  # GET /resource/sign_up
-  def new
+  private
+  def render_not_found
     head :not_found
   end
-
-  # POST /resource
-  def create
-    super
-  end
-
-  # GET /resource/edit
-  def edit
-    head :not_found
-  end
-
-  # PUT /resource
-  def update
-    head :not_found
-  end
-
-  # DELETE /resource
-  def destroy
-    head :not_found
-  end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  def cancel
-    head :not_found
-  end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-  #
-  # private
-
-  # def respond_with(resource, _opts = {})
-  #   if request.method == "POST"
-  #     if resource.persisted?
-  #       render json: {
-  #         status: { code: 201, message: "Signed up sucessfully." },
-  #         data: {
-  #           token: current_token
-  #         }
-  #       }, status: :created
-  #     else
-  #       render json: {
-  #         status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
-  #       }, status: :unprocessable_entity
-  #     end
-  #   end
-  # end
 end
