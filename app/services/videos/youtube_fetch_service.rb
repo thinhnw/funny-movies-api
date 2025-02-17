1f1e2aafd679712667da79f0b6d3a71d6c75c0ec
thinhnw <thinhnv0412@gmail.com>
2025-02-17 16:04:40 +0700

    Add unit tests for YoutubeFetchService

--- app/services/videos/youtube_fetch_service.rb
+++ app/services/videos/youtube_fetch_service.rb
@@ -1,11 +1,17 @@
 module Videos
   class YoutubeFetchService < FetchService
-    @@youtube = Google::Apis::YoutubeV3::YouTubeService.new
-    @@youtube.key = ENV["YOUTUBE_API_KEY"]
+    def initialize(url)
+      super
+      @youtube = Google::Apis::YoutubeV3::YouTubeService.new
+      @youtube.key = ENV["YOUTUBE_API_KEY"]
+    end
 
     def call
       video_id = extract_youtube_video_id(@url)
-      response = @@youtube.list_videos("snippet", id: video_id)
+      response = @youtube.list_videos("snippet", id: video_id)
+      if response.items.empty?
+        raise ArgumentError, "Cannot find Youtube video"
+      end
       format_output title: response.items.first.snippet.title,
                     description: response.items.first.snippet.description
     end
@@ -14,8 +20,8 @@ module Videos
     def extract_youtube_video_id(url)
       regex = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
       match = url.match(regex)
-      unless match
-        raise ArgumentError, "Invalid YouTube URL" unless match
+      unless match && match[2].size == 11
+        raise ArgumentError, "Invalid YouTube URL"
       else
         match[2]
       end
