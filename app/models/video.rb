0e2e57f2f97c0ef9e5c29f32b29466c6d2973f9d
thinhnw <thinhnv0412@gmail.com>
2025-02-17 15:47:44 +0700

    Broadcast to NotificationChannel after video iscreated

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,21 @@
 class Video < ApplicationRecord
+  after_create_commit :broadcast_video_creation
+
   belongs_to :user
   validates :url, presence: true
   scope :ordered, -> { order(created_at: :desc) }
+
+  private
+
+  def broadcast_video_creation
+    ActionCable.server.broadcast(
+      "notification_channel",
+      {
+        id: id,
+        url: url,
+        title: title,
+        user: user.email
+      }
+    )
+  end
 end
