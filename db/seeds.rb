3776e5eb2b3a757eb36cae1e4ac0b2fafa9433fd
thinhnw <thinhnv0412@gmail.com>
2025-02-18 02:46:04 +0700

    Add seeds for development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+  alice = User.find_or_create_by!(email: "alice@foo.bar") do |user|
+    user.password = "password"
+  end
+
+  bob = User.find_or_create_by!(email: "bob@foo.bar") do |user|
+    user.password = "password"
+  end
+
+  alice.videos.create!(
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    title: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus hic perferendis iste suscipit delectus facilis quibusdam dolores similique provident magnam."
+  )
+  bob.videos.create!(
+    url: "https://www.youtube.com/watch?v=aAkMkVFwAoo",
+    title: "Mariah Carey - All I Want for Christmas Is You (Make My Wish Come True Edition)",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus hic perferendis iste suscipit delectus facilis quibusdam dolores similique provident magnam."
+  )
+end
