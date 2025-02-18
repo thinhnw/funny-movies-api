bc0b15847a72cfb0ae8368605e55d734bf75474f
thinhnw <thinhnv0412@gmail.com>
2025-02-18 16:44:13 +0700

    Change authentication method of NotificationChannel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,27 +1,6 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
     def connect
-      self.current_user = find_verified_user
-    end
-
-    protected
-    def find_verified_user
-      token = cookies.to_hash["token"]
-      reject_unauthorized_connection unless token
-      begin
-        decoded_token = JWT.decode(
-          token,
-          ENV["DEVISE_JWT_SECRET_KEY"],
-          true,
-          { algorithm: "HS256" }
-        )
-        user_id = decoded_token.first["sub"]
-        User.find(user_id)
-      rescue
-        reject_unauthorized_connection
-      end
     end
   end
 end
--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,10 +1,30 @@
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
-    reject if current_user.nil?
+    @current_user = find_verified_user
+    reject if @current_user.nil?
     stream_from "notification_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  protected
+  def find_verified_user
+    token = params[:token]
+    reject unless token
+    begin
+      decoded_token = JWT.decode(
+        token,
+        ENV["DEVISE_JWT_SECRET_KEY"],
+        true,
+        { algorithm: "HS256" }
+      )
+      user_id = decoded_token.first["sub"]
+      User.find(user_id)
+    rescue
+      logger.error "Error decoding token: #{e.message}"
+      reject
+    end
   end
 end
