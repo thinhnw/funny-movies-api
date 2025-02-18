d6b83d6e0517d39d26fdb3b1baa56ae31ce6be46
thinhnw <thinhnv0412@gmail.com>
2025-02-18 13:56:49 +0000

    Retrieve allowed origins from environment variables

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
   # end
   allow do
-    origins "funnymovies.vercel.app", "localhost:3001"
+    origins ENV.fetch('ALLOWED_ORIGINS', '').split(',')
     resource "*",
             headers: :any,
             methods: %i[get post put patch delete options head],
